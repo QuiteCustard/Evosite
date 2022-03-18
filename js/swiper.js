c4601f8c6b1847921bc718a95088ef955d6b9469
SamEdwards141 <killercustard12@gmail.com>
2022-03-18 15:12:49 +0000

    Mobile swiper and hover styles

---
+++ js/swiper.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper', {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+    
+});
